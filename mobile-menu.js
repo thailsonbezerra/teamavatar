d03e14826e997f68101c45f024428c2561f20a62
Thailson Bezerra <thailsonbezerra@outlook.com>
2022-06-04 14:49:29 -0300

    mobile menu toggle.

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,5 +1,12 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 
 mobileMenu.addEventListener("click", () => {
-  mobileMenu.classList.toggle("ativo");
+  const header = document.querySelector(".header");
+  header.classList.toggle("ativo");
+
+  if (header.classList.contains("ativo")) {
+    document.body.style.overflowY = "hidden";
+  } else {
+    document.body.style.overflowY = "auto";
+  }
 });
