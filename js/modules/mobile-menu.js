b476765a35b4377bf7dbaccca25106bc0cd9515a
Thailson Bezerra <thailsonbezerra@outlook.com>
2022-06-20 23:40:02 -0300

    teste menu mobile overflow

--- js/modules/mobile-menu.js
+++ js/modules/mobile-menu.js
@@ -7,9 +7,12 @@ export default function initMobileMenu() {
     header.classList.toggle("ativo");
 
     if (header.classList.contains("ativo")) {
-      document.body.style.overflowY = "hidden";
+      // document.body.style.overflow = "hidden";
+      document.body.classList.add("esconder");
     } else {
-      document.body.style.overflowY = "auto";
+      //document.body.style.overflow = "auto";
+      //console.log(document.body.style);
+      document.body.classList.remove("esconder");
     }
   });
 }
